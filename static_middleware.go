dc60bff758f1c9ef79c197acc329bdc05e5cdc4d
mlctrez <mlctrez@gmail.com>
2016-05-03 14:08:14 -0500

    doc fixes

--- static_middleware.go
+++ static_middleware.go
@@ -6,6 +6,9 @@ import (
 	"strings"
 )
 
+// StaticOption configures how StaticMiddlewareDir handles url paths and index files for directories.
+// If set, Prefix is removed from the start of the url path before attempting to serve a directory or file.
+// If set, IndexFile is the index file to serve when the url path maps to a directory.
 type StaticOption struct {
 	Prefix    string
 	IndexFile string
